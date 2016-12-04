7061c539efbd98b9a4239f1239f53573693c78fc
sunbin <sb1169693813>
2016-12-04 22:25:15 +0800

    2016-12-04 22:25
    数据库迁移

---
+++ database/migrations/2016_12_04_141809_create_links_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('links', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+            $table->increments('id')->notNull()->comment('id');
+            $table->string('name',50)->notNull()->comment('链接名字');
+            $table->string('title',50)->default(null)->comment('说明');
+            $table->string('url')->default(null)->comment('链接');
+            $table->integer('order')->default(0)->comment('排序');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
